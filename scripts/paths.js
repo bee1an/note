00f732b79c350a9346201c1912f4f29a4cd693c9
lichanglin <beelan@yeah.net>
2025-03-05 17:00:40 +0800

    Write automated scripts

---
+++ scripts/paths.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const root = path.resolve(__dirname, '..')
+
+const pathSrc = path.join(root, 'src')
+
+module.exports = { root, pathSrc }
---
+++ scripts/redeme-template.js
@@ -0,0 +1,5 @@
+const prefix = `### 随手记
+
+记录在开发过程中遇到的任何东西`
+
+module.exports = { prefix }
